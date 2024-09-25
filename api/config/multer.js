986ccd8c109daca6fe1ad1994af938612a7c8be3
Thakshith <Valmeeki.Thakshith@team.telstra.com>
2024-09-25 20:55:53 +0530

    back to prev

--- api/config/multer.js
+++ api/config/multer.js
@@ -1,26 +1,25 @@
 const multer = require('multer');
-const AWS = require('aws-sdk');
-const multerS3 = require('multer-s3');
+const path = require('path');
+const fs = require('fs');
 
-// Initialize the S3 SDK with your AWS credentials
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Store securely in environment variables
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Store securely in environment variables
-    region: 'us-east-1' // E.g., 'us-east-1'
+// Create the uploads directory if it doesn't exist
+const uploadDir = 'uploads/';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir); // Create the uploads directory
+}
+
+// Configure multer for local file storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir); // Files will be stored in the 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const basename = path.basename(file.originalname, ext);
+        cb(null, `${basename}-${Date.now()}${ext}`); // Generate unique file names
+    }
 });
 
-const s3 = new AWS.S3();
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'customerticket-attachments', // Replace with your S3 bucket name
-        acl: 'public-read', // Files will be publicly accessible via URL
-        key: function (req, file, cb) {
-            const fileName = `${Date.now()}-${file.originalname}`;
-            cb(null, fileName); // File will be saved with this key (filename) in S3
-        }
-    })
-});
+const upload = multer({ storage });
 
 module.exports = upload;
--- api/server.js
+++ api/server.js
@@ -7,14 +7,14 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://54.145.172.167:3000/', // Replace with the URL of your frontend application
+    origin: 'http://54.145.172.167:3000', // Replace with the URL of your frontend application
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
